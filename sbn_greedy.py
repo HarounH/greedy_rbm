dc56e55ca829ef370fb34d14eac154642f8ae8db
Haroun H <haroun7@gmail.com>
2018-01-20 16:21:44 -0600

    Only have to write selector function now

--- sbn_greedy.py
+++ sbn_greedy.py
@@ -1,4 +1,7 @@
-
+'''
+    Implementation of SBN such that
+    we sparsify latent variables
+'''
 __author__ = 'haroun habeeb'
 __mail__ = 'haroun7@gmail.com'
 
@@ -89,13 +92,41 @@ class SBN_Greedy(nn.Module):
                 ----
             '''
             batch_size = xin.size()[0]
-            q = smooth_distribution(self.x2z(xin))
-            samples_z, sampler_z = self.sample_from(q, S)
+            q = smooth_distribution(self.x2z(xin))  # batch size, nz
+            samples_z, sampler_z = self.sample_from(q, S)  # S, batch_size, nz
             ps = torch.stack([smooth_distribution(self.z2x(sample_z)) for samples_z])
             xouts = torch.stack([self.sample_from(psi, 1)[0][0] for psi in ps])
             if compute_loss:
-                
+                # Need to compute which variables are free
 
+                # Get which variables are free
+                # binary vector of length nz
+                Z_f_indices, Z_f_c_indices = self.get_k_free_latent_variables(k, xin)  # batch_size, nz
+                # Compute elbo
+                self.set_latent_variables_constants(samples_z, Z_f_c_indices, q.expand(S, batch_size, self.nz))
+
+
+                logq_zi = torch.stack([sampler_z.log_prob(sample_z) for sample_z in samples_z])  # S, batch_size, nz
+                pz = smooth_distribution(F.sigmoid(self.z_bias).expand(S, batch_size, -1))
+                z_prior = Bernoulli(pz)
+                logp_zi = z_prior.log_prob(samples_z)
+                ps_new = torch.stack([smooth_distribution(self.z2x(sample_z)) for sample_z in samples_z])
+                px_given_z = Bernoulli(ps_new)
+                logp_xin_given_samples_z = px_given_z.log_prob(xin.expand(S, batch_size, self.nx))  # S, batch_size, nx
+
+                # Now to put it all together
+                logq = logq_zi.sum(dim=2)
+                logp = (logp_zi + logp_xin_given_samples_z).sum(dim=2)
+                term2 = self.compute_term1(logq, logq)  # Eh, the names can be fixed later
+                term1 = self.compute_term1(logq, logp)
+                loss = -(term1 - term2)
             else:
                 loss = None
             return q, samples_z, ps, xouts, loss
+
+    def set_latent_variables_constants(self, inp, indices, bernoulli_parameters):
+        '''
+            inp: S, batch_size, n sized
+            sets indices along dim=2 to constants that we need to figure out still
+        '''
+        inp[:, :, indices] = bernoulli_parameters[:, :, indices] > 0.5
